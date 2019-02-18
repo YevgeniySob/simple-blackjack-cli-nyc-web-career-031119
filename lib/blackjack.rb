82f2ae7671630943e7794049cb2a776a785c2618
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-18 21:55:39 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,33 +1,43 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  rand(10) + 1
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(num)
+  puts "Your cards add up to #{num}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  value = gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(num)
+  puts "Sorry, you hit 27. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  sum = 0
+  sum += deal_card
+  sum += deal_card
+  display_card_total(sum)
+  return sum
 end
 
-def hit?
-  # code hit? here
+def hit?(num)
+  prompt_user
+  value = get_user_input
+  if value = "s"
+    return num
+  elsif value == "h"
+    return deal_card + num
+  end
 end
 
 def invalid_command
